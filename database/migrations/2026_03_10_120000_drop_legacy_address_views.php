889191ec0fc2ed92cbb2c89c60691ea2904e4ba7
Edinei Valdameri <edineivaldameri@gmail.com>
2026-03-11 21:00:51 -0300

    Remove o método down devido a exclusão dos arquivos

--- database/migrations/2026_03_10_120000_drop_legacy_address_views.php
+++ database/migrations/2026_03_10_120000_drop_legacy_address_views.php
@@ -17,14 +17,4 @@ return new class extends Migration
         $this->dropView('public.pais');
         $this->dropView('public.uf');
     }
-
-    public function down(): void
-    {
-        $this->createView('public.uf');
-        $this->createView('public.pais');
-        $this->createView('public.municipio');
-        $this->createView('public.distrito');
-        $this->createView('public.bairro');
-        $this->createView('cadastro.endereco_pessoa');
-    }
 };
