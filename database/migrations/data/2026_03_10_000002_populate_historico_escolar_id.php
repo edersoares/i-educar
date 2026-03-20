e9d01839f7e5716fbc4507712400279160520278
Robert Ferraz <robert@portabilis.com.br>
2026-03-20 13:26:47 -0300

    Otimizada migration de dados com DISABLE TRIGGER ALL

--- database/migrations/data/2026_03_10_000002_populate_historico_escolar_id.php
+++ database/migrations/data/2026_03_10_000002_populate_historico_escolar_id.php
@@ -1,12 +1,19 @@
 <?php
 
+use App\Support\Database\EnableDisableForeignKeys;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
+    use EnableDisableForeignKeys;
+
+    public $withinTransaction = false;
+
     public function up(): void
     {
+        $this->disableForeignKeys('pmieducar.historico_disciplinas');
+
         DB::statement('
             UPDATE pmieducar.historico_disciplinas hd
             SET historico_escolar_id = he.id
@@ -14,6 +21,8 @@ return new class extends Migration
             WHERE hd.ref_ref_cod_aluno = he.ref_cod_aluno
               AND hd.ref_sequencial = he.sequencial
         ');
+
+        $this->enableForeignKeys('pmieducar.historico_disciplinas');
     }
 
     public function down(): void
