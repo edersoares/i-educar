10e2f56ededdc86191f65364e375818d0d0bf334
Robert Ferraz <robert@portabilis.com.br>
2026-04-20 09:55:03 -0300

    Separada insercao de menu em migration de data

--- database/migrations/2026_02_24_120000_create_component_batch_manager.php
+++ database/migrations/2026_02_24_120000_create_component_batch_manager.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Menu;
-use App\Process;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,20 +18,10 @@ return new class extends Migration
             $table->timestamps();
             $table->index(['user_id', 'created_at']);
         });
-
-        Menu::query()->updateOrCreate(['old' => Process::COMPONENT_BATCH_MANAGER], [
-            'parent_id' => Menu::query()->where('old', Process::CONFIGURATIONS_TOOLS)->firstOrFail()->getKey(),
-            'process' => Process::COMPONENT_BATCH_MANAGER,
-            'title' => 'Gerenciamento em Lote de Componentes',
-            'order' => 0,
-            'parent_old' => Process::CONFIGURATIONS_TOOLS,
-            'link' => '/gerenciamento-componentes',
-        ]);
     }
 
     public function down(): void
     {
         Schema::dropIfExists('component_batch_operations');
-        Menu::query()->where('old', Process::COMPONENT_BATCH_MANAGER)->delete();
     }
 };
